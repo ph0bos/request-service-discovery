df3bf95d005174c64aae645bc595b28b73f3b594
Steven Robinson <sm0k3yuk@gmail.com>
2015-10-27 07:36:14 +0000

    Add tests using mocked zoologist

--- examples/index.js
+++ examples/index.js
@@ -5,8 +5,7 @@ var RequestServiceDiscovery = require('../');
 var client = new RequestServiceDiscovery({
   connectionString: '127.0.0.1:2181',
   basePath: 'services',
-  serviceName: 'content/sport/repository/v1',
-  verbose: false
+  serviceName: 'content/sport/repository/v1'
 });
 
 client.get('sport', null, function(err, res) {
--- test/request-service-discovery.js
+++ test/request-service-discovery.js
@@ -1,31 +1,86 @@
 'use strict';
 
-var chai   = require('chai').should();
-var mocha  = require('mocha');
-var sinon  = require('sinon');
+var chai    = require('chai').should();
+var mocha   = require('mocha');
+var sinon   = require('sinon');
+var mockery = require('mockery');
 
 var RequestServiceDiscovery = require('../lib/request-service-discovery');
-var instance = null;
 
 describe('request-service-discovery', function () {
   before(function(done) {
-    instance = new RequestServiceDiscovery({
-      basePath: 'services',
-      serviceName: 'test/service/v1',
-      connectionString: '127.0.0.1:2181'
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false
     });
 
     done();
   });
 
-  after(function(done) {
+  afterEach(function(done) {
+    mockery.disable();
     done();
   });
 
-  it('calling get() should throw an error', function (done) {
+  it('calling get() when the zoologist is not connected it should throw an error', function (done) {
+    mockery.registerMock('./zoologist', _createDisconnectedZoologistMock());
+
+    var instance = _createInstance();
+
     instance.get('resource-name/1234', null, function(err, result) {
       err.should.be.defined;
       done();
     });
   });
+
+  it('calling put() when the zoologist is not connected it should throw an error', function (done) {
+    mockery.registerMock('./zoologist', _createDisconnectedZoologistMock());
+
+    var instance = _createInstance();
+
+    instance.put('resource-name/1234', null, null, function(err, result) {
+      err.should.be.defined;
+      done();
+    });
+  });
+
+  it('calling post() when the zoologist is not connected it should throw an error', function (done) {
+    mockery.registerMock('./zoologist', _createDisconnectedZoologistMock());
+
+    var instance = _createInstance();
+
+    instance.post('resource-name', null, null, function(err, result) {
+      err.should.be.defined;
+      done();
+    });
+  });
+
+  it('calling delete() when the zoologist is not connected it should throw an error', function (done) {
+    mockery.registerMock('./zoologist', _createDisconnectedZoologistMock());
+
+    var instance = _createInstance();
+
+    instance.delete('resource-name/1234', null, function(err, result) {
+      err.should.be.defined;
+      done();
+    });
+  });
 });
+
+function _createInstance() {
+  return new RequestServiceDiscovery({
+    basePath: 'services',
+    serviceName: 'test/service/v1',
+    connectionString: '127.0.0.1:2181'
+  });
+}
+
+function _createDisconnectedZoologistMock() {
+  return {
+    init: function() {
+    },
+    isConnected: function() {
+      return false;
+    }
+  };
+}
